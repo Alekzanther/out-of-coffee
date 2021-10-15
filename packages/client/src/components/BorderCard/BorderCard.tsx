3dbfe4b934f9ca07e3da3dbd32f78252b200c55e
Victor Ek <viceki@antura.se>
2021-10-15 14:30:42 +0200

    add small version to border card

--- packages/client/src/components/BorderCard/BorderCard.tsx
+++ packages/client/src/components/BorderCard/BorderCard.tsx
@@ -1,13 +1,15 @@
 import styles from './BorderCard.module.css';
 
 interface Props {
-  title: string;
+  title?: string;
   subTitle: string;
 }
 
 export const BorderCard: React.FC<Props> = ({title, subTitle, children}) => {
+  const padding = title ? '48px' : '16px'
+
   return (
-    <div className={styles.borderCard}>
+    <div className={styles.borderCard} style={{padding}}>
       <div className={styles.cardTitlesContainer}>
         <div className={styles.cardTitles}>
             {/* TODO: Add typography component */}
--- packages/client/src/views/Orders.tsx
+++ packages/client/src/views/Orders.tsx
@@ -3,7 +3,7 @@ import { BorderCard } from "../components"
 export const Orders = () => {
     return (
         <div style={{display: 'flex', justifyContent: 'center'}}>
-            <BorderCard title="Nästa order" subTitle="levereras snart kanske">
+            <BorderCard subTitle="levereras snart kanske">
                 Beställningar
                 <br />
                 Beställningar
