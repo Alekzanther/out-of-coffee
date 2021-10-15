e2095c5c7f934eb7af26631e916ec3ff1f7c74b7
Mattias Rådemar <mattias.rademar@antura.se>
2021-10-15 16:11:59 +0200

    Extract theme and global styles

---
+++ packages/client/src/globalStyles.ts
@@ -0,0 +1,8 @@
+import {css} from "@emotion/react"
+
+export const globalStyles = css`
+p {
+  weight: 400;
+  font-size: 14px;
+}
+`;
---
+++ packages/client/src/theme.tsx
@@ -0,0 +1,8 @@
+import { Theme } from '@emotion/react';
+
+export const theme: Theme = {
+  colors: {
+    greenLantern: 'rgba(25, 122, 23, 0.54)',
+    pitchBlack: '#000000',
+  },
+};
