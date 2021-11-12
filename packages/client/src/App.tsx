5523e3a4daf0aad047da590fc4281a4124d4bef5
Michael Zetterholm <michael.zetterholm@antura.se>
2021-11-12 15:36:20 +0100

    Add autogenerate queries

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,14 @@
 import { Global, ThemeProvider } from '@emotion/react';
+import { useGetSchemaQuery } from 'generated/graphql';
 import { globalStyles } from 'globalStyles';
 
 import { theme } from 'theme';
 import { Header, Nav } from './components';
+
 function App() {
+  const { data, error } = useGetSchemaQuery();
+  console.log(`data`, data);
+  console.log(`error`, error);
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalStyles} />
