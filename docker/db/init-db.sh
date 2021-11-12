236340fa594bc7242eb913f9135bf308b56d45ba
Michael Zetterholm <michael.zetterholm@antura.se>
2021-11-12 13:47:46 +0100

    Setup mongo in docker

--- docker/db/init-db.sh
+++ docker/db/init-db.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 set -e
+# set -e causes the whole script to exit when a command fails, so the script can't silently fail and startup mongo.
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-		CREATE TABLE IF NOT EXISTS items (
-					id INTEGER UNIQUE PRIMARY KEY,
-					name TEXT UNIQUE NOT NULL,
-					link TEXT,
-					category TEXT
-		);
-		CREATE TABLE IF NOT EXISTS base_order (
-					item_id INTEGER PRIMARY KEY REFERENCES items(id),
-					quantity TEXT,
-					frequency_per_week INTEGER default 1
-		);
-		CREATE TABLE IF NOT EXISTS orders (
-					id SERIAL PRIMARY KEY UNIQUE,
-					order_date TEXT UNIQUE,
-					status TEXT
-		);
-		CREATE TABLE IF NOT EXISTS order_items (
-					item_id INTEGER REFERENCES items(id),
-					order_id INTEGER REFERENCES orders(id),
-					quantity TEXT,
-					PRIMARY KEY (item_id, order_id)
-		);
-EOSQL
+mongosh <<EOF
+use admin
+db.createUser(
+  {
+    user: "${MONGO_INITDB_ROOT_USERNAME}",
+    pwd: "${MONGO_INITDB_ROOT_PASSWORD}",
+    roles: [ { role: "userAdminAnyDatabase", db: "admin" }, "readWriteAnyDatabase" ]
+  }
+)
+
+use ${MONGO_INITDB_DATABASE}
+db.createCollection("orders")
+db.createCollection("items")
+db.createCollection("base_orders")
+EOF
