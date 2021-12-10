878bc7b801805f82bb38f87b7693bb76a2cacede
Michael Zetterholm <michael.zetterholm@antura.se>
2021-12-10 14:10:30 +0100

    Move BaseOrders to its own file

--- packages/client/src/views/favorites/Favorites.tsx
+++ packages/client/src/views/favorites/Favorites.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { css } from '@emotion/react';
 
 import { SimpleList } from '../../components';
-import { useGetBaseOrderQuery } from 'generated/graphql';
+import { BaseOrdersList } from './BaseOrdersList';
 
 type FavoriteItem = {
   title: string;
@@ -26,7 +26,7 @@ const wrapperStyle = css`
   justify-content: space-around;
 `;
 
-const listWrapperStyle = css`
+export const listWrapperStyle = css`
   max-height: 500px;
   display: flex;
   flex-direction: column;
@@ -35,10 +35,6 @@ const listWrapperStyle = css`
 `;
 
 export const Favorites: React.FC = () => {
-  const { data } = useGetBaseOrderQuery();
-
-  const items = data?.GetBaseOrder?.items || [];
-
   return (
     <div css={wrapperStyle}>
       <div css={listWrapperStyle}>
@@ -51,47 +47,7 @@ export const Favorites: React.FC = () => {
           />
         ))}
       </div>
-      <div css={listWrapperStyle}>
-        <h2>Basorder</h2>
-        <ul style={{ listStyle: 'none' }}>
-          {items.length >= 1
-            ? items.map((item, index) => (
-                <li key={index}>
-                  <p>{item?.name}</p>
-                  {item?.productImageUrl ? (
-                    <a
-                      style={{
-                        textDecoration: 'none',
-                      }}
-                      href={item?.productUrl}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <img
-                        style={{ height: '50px', width: '50px' }}
-                        src={item?.productImageUrl}
-                        alt={item.name}
-                      />
-                    </a>
-                  ) : (
-                    <>
-                      {item?.productUrl && (
-                        <a
-                          style={{
-                            textDecoration: 'none',
-                          }}
-                          href={item?.productUrl}
-                        >
-                          {`Gå till ${item.name}`}
-                        </a>
-                      )}
-                    </>
-                  )}
-                </li>
-              ))
-            : null}
-        </ul>
-      </div>
+      <BaseOrdersList />
     </div>
   );
 };
