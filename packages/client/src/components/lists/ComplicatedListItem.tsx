c2247ce5183ea215e18a5f15152f5a25e99be4bd
Michael Zetterholm <michael.zetterholm@antura.se>
2022-05-20 14:25:40 +0200

    Remove forwardRef from ComplicatedLIstItem

--- packages/client/src/components/lists/ComplicatedListItem.tsx
+++ packages/client/src/components/lists/ComplicatedListItem.tsx
@@ -59,81 +59,77 @@ const linkStyle = css`
   text-decoration: 'none';
 `;
 
-export const ComplicatedListItem = forwardRef<
-  HTMLDivElement,
-  ComplicatedListItemProps
->(
-  ({
-    item,
-    newItem,
-    throwItInTheTrash,
-    incrementAndDecrementItems,
-    id,
-    setSelectedProduct,
-  }) => {
-    const [setFave] = useSetFavoriteMutation({
-      variables: {
-        id: item._id,
-        value: Boolean(item.isFavorite),
-      },
-    });
+export const ComplicatedListItem = ({
+  item,
+  newItem,
+  throwItInTheTrash,
+  incrementAndDecrementItems,
+  id,
+  setSelectedProduct,
+}: ComplicatedListItemProps) => {
+  const [setFave] = useSetFavoriteMutation({
+    variables: {
+      id: item._id,
+      value: Boolean(item.isFavorite),
+    },
+  });
 
-    const [position, setPosition] = useState<string[]>([]);
-    // console.log('position', position);
+  const [position, setPosition] = useState<string[]>([]);
+  // console.log('position', position);
 
-    const measuredRef = useCallback((node) => {
-      if (node !== null) {
-        const x = node.getBoundingClientRect().x;
-        const y = node.getBoundingClientRect().y;
-        setPosition([x.toString(), y.toString()]);
-      }
-    }, []);
+  const measuredRef = useCallback((node) => {
+    if (node !== null) {
+      const x = node.getBoundingClientRect().x;
+      const y = node.getBoundingClientRect().y;
+      setPosition([x.toString(), y.toString()]);
+    }
+  }, []);
 
-    const setFavorite = () => {
-      setFave();
-    };
+  const setFavorite = () => {
+    setFave();
+  };
 
-    const handleOnAdd = () => {
-      setSelectedProduct(id, position, item);
-    };
+  const handleOnAdd = () => {
+    setSelectedProduct(id, position, item);
+  };
 
-    return (
-      <div id={id} css={style}>
-        {newItem ? <New /> : null}
-        {item?.productImageUrl ? (
-          <a
-            css={linkStyle}
-            href={item?.productUrl}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              ref={measuredRef}
-              style={{ height: '50px', width: '50px' }}
-              src={item?.productImageUrl}
-              alt={item.name}
-            />
-          </a>
-        ) : (
-          <>
-            {item?.productUrl && (
-              <a css={linkStyle} href={item?.productUrl}>
-                {`Gå till ${item.name}`}
-              </a>
-            )}
-          </>
-        )}
-        <p
-          css={(theme) => ({
-            color: newItem
-              ? theme.colors.greenLantern
-              : theme.colors.pitchBlack,
-          })}
+  return (
+    <div id={id} css={style}>
+      {newItem ? <New /> : null}
+      {item?.productImageUrl ? (
+        <a
+          css={linkStyle}
+          href={item?.productUrl}
+          target="_blank"
+          rel="noreferrer"
         >
-          {item.name}
-        </p>
-        <div css={actionsContainer}>
-          {/* {!incrementAndDecrementItems && (
+          <img
+            ref={measuredRef}
+            style={{ height: '50px', width: '50px' }}
+            src={item?.productImageUrl}
+            alt={item.name}
+          />
+        </a>
+      ) : (
+        <>
+          {item?.productUrl && (
+            <a css={linkStyle} href={item?.productUrl}>
+              {`Gå till ${item.name}`}
+            </a>
+          )}
+        </>
+      )}
+      <p
+        css={(theme) => ({
+          color: newItem
+            ? theme.colors.greenLantern
+            : theme.colors.pitchBlack,
+        })}
+      >
+        {item.name}
+      </p>
+      <div css={actionsContainer}>
+        {/* {!incrementAndDecrementItems && (
           <div css={incrementContainerStyles}>
             <button
               aria-label="decrease"
@@ -157,28 +153,27 @@ export const ComplicatedListItem = forwardRef<
             </button>
           </div>
         )} */}
-          <button onClick={handleOnAdd}>Lägg till</button>
-          {throwItInTheTrash && (
-            <div css={incrementContainerStyles}>
-              <button
-                type="button"
-                css={nakedButtonStyles}
-                aria-label="remove"
-              >
-                <Trashcan style={{ cursor: 'pointer' }} />
-              </button>
-            </div>
-          )}
+        <button onClick={handleOnAdd}>Lägg till</button>
+        {throwItInTheTrash && (
           <div css={incrementContainerStyles}>
-            <AnimatedButton
-              kind="outlineHeartToFilledHeart"
-              onClick={setFavorite}
-              ariaLabel="favorite"
-              isToggled={item.isFavorite}
-            />
+            <button
+              type="button"
+              css={nakedButtonStyles}
+              aria-label="remove"
+            >
+              <Trashcan style={{ cursor: 'pointer' }} />
+            </button>
           </div>
+        )}
+        <div css={incrementContainerStyles}>
+          <AnimatedButton
+            kind="outlineHeartToFilledHeart"
+            onClick={setFavorite}
+            ariaLabel="favorite"
+            isToggled={item.isFavorite}
+          />
         </div>
       </div>
-    );
-  },
-);
+    </div>
+  );
+};
