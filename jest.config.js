aab1fbfc59240a62eca5399f196c7ee7abbc6708
Michael Zetterholm <michael.zetterholm@antura.se>
2022-10-07 16:07:34 +0200

    Transform jsx with jest config instead of babelconfig

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,12 @@ const config = {
     'mts',
     'cts',
   ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'babel-jest',
+      { presets: ['next/babel'] },
+    ],
+  },
 };
 
 module.exports = config;
