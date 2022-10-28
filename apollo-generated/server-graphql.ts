92ec3ac180496620bff256a24b92f2ee31a5d757
Michael Zetterholm <michael.zetterholm@antura.se>
2022-10-28 14:30:44 +0200

    Den skulle med också

--- apollo-generated/server-graphql.ts
+++ apollo-generated/server-graphql.ts
@@ -119,11 +119,6 @@ export type Query = {
 };
 
 
-export type QueryGetCurrentOrderArgs = {
-  id: Scalars['ID'];
-};
-
-
 export type QueryGetItemArgs = {
   id: Scalars['ID'];
 };
@@ -334,7 +329,7 @@ export type OrderResolvers<ContextType = any, ParentType extends ResolversParent
 
 export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
   GetBaseOrder?: Resolver<ResolversTypes['BaseOrder'], ParentType, ContextType>;
-  GetCurrentOrder?: Resolver<ResolversTypes['Order'], ParentType, ContextType, RequireFields<QueryGetCurrentOrderArgs, 'id'>>;
+  GetCurrentOrder?: Resolver<ResolversTypes['Order'], ParentType, ContextType>;
   GetFavoriteItems?: Resolver<Array<ResolversTypes['Item']>, ParentType, ContextType>;
   GetItem?: Resolver<ResolversTypes['Item'], ParentType, ContextType, RequireFields<QueryGetItemArgs, 'id'>>;
   GetItems?: Resolver<Array<ResolversTypes['Item']>, ParentType, ContextType>;
