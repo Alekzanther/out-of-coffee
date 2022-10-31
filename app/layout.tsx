e3e888ee5915780feb285d375f6c2fea31f53b85
Alekzanther <alexanderlndbrg@gmail.com>
2022-10-31 21:54:32 +0100

    Use proper layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import './global.css';
 
 export default function RootLayout({
@@ -11,11 +12,23 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>Mysliy0</title>
+        <link
+          href="https://res.cloudinary.com/dr6lvwubh/raw/upload/v1581441981/Anicons/anicons-regular.css"
+          rel="stylesheet"
+        />;
       </head>
       <body>
-        <div className="app">
-          {children}
+        <div>
+          <p>Beställ innan torsdag 25 september kl 12:00! 🎉</p>
         </div>
+        <div>
+          <Link href="/">hem</Link>
+          <Link href="/products">products</Link>
+          <Link href="/favorites">favorites</Link>
+          <Link href="/stuff">stuff</Link>
+        </div>
+        <h1>Müsl.io</h1>
+        <div>{children}</div>
       </body>
     </html>
   );
