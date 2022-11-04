8d6cc3b2c6fcb7521fed32e2683ee5849e1842d1
Michael Zetterholm <michael.zetterholm@antura.se>
2022-11-04 13:33:41 +0100

    Fix image urls in mock data

--- docker/db/init-db.sh
+++ docker/db/init-db.sh
@@ -17,11 +17,11 @@ db.createCollection("orders")
 db.createCollection("items")
 db.createCollection("baseorders")
 let itemsArr = [
- {"name":"Snabbnudlar Kycklingsmak 85g Samyang","productUrl":"https://mathem.se/varor/snabbnudlar/snabbnudlar-kyckling-85g-samyang-95873","productImageUrl":"//static.mathem.se/shared/images/products/original/07350035210107_c1n1.jpg","isFavorite":false,"mathemId":"95873"},
- {"name":"Chips Sourcream & Onion 275g Estrella","productUrl":"https://mathem.se/varor/chips-med-smak/chips-sourcream-o-onion-300g-estrella","productImageUrl":"//static.mathem.se/shared/images/products/original/chips-sourcream-o-onion-275g-estrella.jpeg","isFavorite":false,"mathemId":"14424"},
- {"name":"Kaffe Brygg Mellanrost 450g Gevalia","productUrl":"https://mathem.se/varor/bryggkaffe/kaffe-brygg-mellanrost-500g-gevalia","productImageUrl":"//static.mathem.se/shared/images/products/original/08711000530085_c1r1.jpeg.jpg","isFavorite":false,"mathemId":"14804"},
- {"name":"Färsk Mellanmjölk 1,5% 1,5L Arla","productUrl":"https://mathem.se/varor/mellanmjolk/mellanmjolk-1-5--1-5l-arla","productImageUrl":"//static.mathem.se/shared/images/products/original/farsk-mellanmjolk-1-5--1-5l-arla-2.jpg","isFavorite":false,"mathemId":"15664"},
- {"name":"Ägg Frigående Inomhus 15-p S 762g Kronägg","productUrl":"https://mathem.se/varor/agg/agg-s-ib-15-p-kronagg-ab","productImageUrl":"//static.mathem.se/shared/images/products/original/agg-frigaende-inomhus-15-p-s-762g-kronagg-2.jpg","isFavorite":false,"mathemId":"99980"}
+ {"name":"Snabbnudlar Kycklingsmak 85g Samyang","productUrl":"https://mathem.se/varor/snabbnudlar/snabbnudlar-kyckling-85g-samyang-95873","productImageUrl":"https://static.mathem.se/shared/images/products/original/07350035210107_c1n1.jpg","isFavorite":false,"mathemId":"95873"},
+ {"name":"Chips Sourcream & Onion 275g Estrella","productUrl":"https://mathem.se/varor/chips-med-smak/chips-sourcream-o-onion-300g-estrella","productImageUrl":"https://static.mathem.se/shared/images/products/original/chips-sourcream-o-onion-275g-estrella.jpeg","isFavorite":false,"mathemId":"14424"},
+ {"name":"Kaffe Brygg Mellanrost 450g Gevalia","productUrl":"https://mathem.se/varor/bryggkaffe/kaffe-brygg-mellanrost-500g-gevalia","productImageUrl":"https://static.mathem.se/shared/images/products/original/08711000530085_c1r1.jpeg.jpg","isFavorite":false,"mathemId":"14804"},
+ {"name":"Färsk Mellanmjölk 1,5% 1,5L Arla","productUrl":"https://mathem.se/varor/mellanmjolk/mellanmjolk-1-5--1-5l-arla","productImageUrl":"https://static.mathem.se/shared/images/products/original/farsk-mellanmjolk-1-5--1-5l-arla-2.jpg","isFavorite":false,"mathemId":"15664"},
+ {"name":"Ägg Frigående Inomhus 15-p S 762g Kronägg","productUrl":"https://mathem.se/varor/agg/agg-s-ib-15-p-kronagg-ab","productImageUrl":"https://static.mathem.se/shared/images/products/original/agg-frigaende-inomhus-15-p-s-762g-kronagg-2.jpg","isFavorite":false,"mathemId":"99980"}
 ]
 
 db.items.insertMany(itemsArr)
